f119856fc4517feb8dfb804c14bf4d28979d69c6
Ugo <courcheugo@gmail.com>
2025-06-19 22:29:07 +0200

    init project

---
+++ ChromaCore/Source/ChromaCore/ChromaCoreGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChromaCoreGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChromaCoreGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AChromaCoreGameMode();
+};
+
+
+
